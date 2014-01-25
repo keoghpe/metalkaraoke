32858f4b94f0beb660b390dabc1bf57e19cf77c8
Peter Keogh <keoghpe@tcd.ie>
2014-01-25 02:21:26 +0000

    Got Karaoke working like a beast... Beer time!!!

--- js/script.js
+++ js/script.js
@@ -1,23 +1,53 @@
 var iframeElement   = document.querySelector('iframe');
 var widget          = SC.Widget(iframeElement);
+var i=0;
+/*
 var theLyrics		= {
 	line1: {
 		timeStamp: 4500,
 		lyric: 'I lie in a bed of snakes'
 	}
+}*/
+loadJSON('js/where_we_call_home.json',
+         function(data) { karaoke(data); },
+         function(xhr) { console.error(xhr); }
+);
+
+function karaoke (theData) {
+    widget.bind(SC.Widget.Events.PLAY, function(){
+        widget.bind(SC.Widget.Events.PLAY_PROGRESS, function() {
+           
+            widget.getPosition(function(position){
+
+                if (position >= theData.theLyrics[i].timeStamp - 200 && position <= theData.theLyrics[i].timeStamp + 200) {
+                    console.log(theData.theLyrics[i].lyric);
+                    document.getElementsByTagName("H1")[0].innerHTML=theData.theLyrics[i].lyric;
+                    i++;
+                };
+
+
+            });
+        });
+    });   
 }
 
 
-widget.bind(SC.Widget.Events.PLAY, function(){
-
-	widget.bind(SC.Widget.Events.PLAY_PROGRESS, function() {
-		var marker = 4500;
-		widget.getPosition(function(position){
-
-			if (position >= theLyrics.line1.timeStamp - 200 && position <= theLyrics.line1.timeStamp + 200) {
-				console.log(theLyrics.line1.lyric);
-			};
-
-		});
-	});
-});
+//This is from http://stackoverflow.com/questions/9838812/how-can-i-open-a-json-file-in-javascript-without-jquery
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));    
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
